61b16435313e7bc05b329f8222dce629c67827d4
Tony Okeke <tonykabilanokeke@gmail.com>
2024-05-02 21:17:53 -0400

    fix path to logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar">
         <NavLink to="/">
-          <img alt="MongoDB logo" className="nav-logo" src="https://raw.githubusercontent.com/mongodb-developer/mern-stack-example/603144e25ba5549159d1962601337652a7bfa253/mern/client/src/assets/mongodb.svg"></img>
+          <img alt="MongoDB logo" className="nav-logo" src="/mongodb.svg"></img>
         </NavLink>
 
         <NavLink className="button" to="/create">
